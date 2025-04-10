40251789b1b075cdf3d9898c8228b22d01c3bac3
mahdizargarzadeh <mahdizargarzadeh0@gmail.com>
2025-04-10 08:04:26 +0330

    transferring templates and static files

--- chat/views.py
+++ chat/views.py
@@ -6,3 +6,24 @@ from django.views import View
 class Main(View):
     def get(self, request):
         return render(request, 'chat/main.html')
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'chat/login.html')
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'chat/register.html')
+
+class Logout(View):
+    def get(self, request):
+        # run operator
+        pass
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'chat/home.html')
+
+class ChatPerson(View):
+    def get(self, request):
+        return render(request, 'chat/chat_person.html')
